ef9772d616b4f82836f62461fdb4c9ce2293bdf9
Jesus <jesus@jesus-M2420.(none)>
2013-12-13 21:48:42 -0430

    Corrigiendo manejo de errores, ahora reconoce los errores por palabras

--- CGinterfaz.py
+++ CGinterfaz.py
@@ -57,8 +57,8 @@ class Interfaz:
     def t_error(t):
         error=open('errorLexico.cg', 'a')
         if t.value!=True:
-          print "Carácter ilegal: '%s'" %  t.value[0] + " en la linea " + str(t.lineno)
-          error.write("Carácter ilegal: '%s'" %  t.value[0] + " en la linea " + str(t.lineno))
+          #print "Carácter ilegal: '%s'" %  t.value[0] + " en la linea " + str(t.lineno)
+          error.write(t.value[0] + ":" + str(t.lineno) + ":" + str(t.lexpos))
           error.write('\n')
         error.close()
         t.lexer.skip(1)
@@ -257,6 +257,10 @@ gtk.STOCK_SAVE, gtk.RESPONSE_OK))
         return item_factory.get_widget("<main>")
 
     def mostrarresultados(self, textbuffer):
+        completo = ""
+        posicion = -1
+        nolinea = -1
+        
         textbuffer = textbuffer
         #textbuffer1 = textbuffer1
         textbuffer.delete(textbuffer.get_start_iter(), textbuffer.get_end_iter())
@@ -264,17 +268,49 @@ gtk.STOCK_SAVE, gtk.RESPONSE_OK))
 
         resultado1 = ['errorLexico.cg']
         resultado2 = ['tokens.cg']
-
         label = gtk.Label()
         n_error = 0
-
+        
+        #Tratamiento de errores
         try:
-            for codigo in resultado1:
-              f = open(codigo, 'r')
-              if f:
-                string = f.read()
-                f.close()
-                textbuffer.set_text(string)
+          f = open ("errorLexico.cg",'r')
+          errores=open('erroresLexico.cg', 'a')
+          for line in f.readlines():
+            separados = line.split(':',3)
+            if posicion != -1:
+              if (posicion + 1) == int(separados[2]):
+                completo += str(separados[0])
+              else:
+                print "Error encontrado: ",completo
+                print "Linea: ",nolinea 
+                errores.write("Error: %s" % completo + " en la linea numero " + str(nolinea))
+                errores.write('\n')
+                completo = ""
+                completo += str(separados[0]) 
+              posicion = int(separados[2])
+              nolinea = int(separados[1])
+            else:
+              posicion = int(separados[2])
+              nolinea = int(separados[1])
+              completo += str(separados[0])
+          if completo != "":
+            print "Error encontrado: ",completo
+            print "Linea: ",nolinea 
+            errores.write("Error: %s" % completo + " en la linea numero " + str(nolinea))
+            errores.write('\n')
+            completo = ""
+          errores.close()
+          f.close()
+        except IOError :
+          print "El Analizador Lexico no encontro errores"    
+         
+        try:
+            #for codigo in resultado1:
+            f = open("erroresLexico.cg", 'r')
+            if f:
+              string = f.read()
+              f.close()
+              textbuffer.set_text(string)
 
             n_error = textbuffer.get_line_count()-1
             label.set_text(str(n_error)+" Errores")
