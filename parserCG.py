6491d0e42f85b6ee700db9f1601367dba2c095b7
Jesus <jesus@jesus-M2420.(none)>
2014-02-03 23:20:49 -0430

    Añadiendo analizador lexico

---
+++ parserCG.py
@@ -0,0 +1,101 @@
+import ply.yacc as yacc
+import lexico
+from lexico import tokens
+import sys
+
+VERBOSE = 1
+
+def p_programa(p):
+	'programa : Inicio lista_sentencia Fin'
+	pass
+
+def p_lista_sentencia_1(p):
+	'lista_sentencia : lista_sentencia sentencia'
+	pass
+
+def p_lista_sentencia_2(p):
+	'lista_sentencia : sentencia'
+	pass
+
+def p_sentencia(p):
+	'''sentencia : definicion 
+				  | asignacion
+                                  | accion '''
+	pass
+
+def p_definicion(p):
+	'definicion : Accion Identificador Reservado Tipo Delimitador'
+	pass
+
+def p_asignacion_1(p):
+	'asignacion : Reservado Propiedad Atributo Reservado Identificador Asignacion Valor Delimitador'
+	pass
+
+def p_asignacion_2(p):
+	'asignacion : Reservado Propiedad Atributo Reservado Identificador Asignacion Identificador Delimitador'
+	pass
+
+def p_asignacion_3(p):
+	'asignacion : Reservado Atributo Reservado Identificador Asignacion Valor Delimitador'
+	pass
+
+def p_asignacion_4(p):
+	'asignacion : Reservado Atributo Reservado Identificador Asignacion Identificador Delimitador'
+	pass
+
+def p_accion(p):
+	'''accion : rotacion 
+				  | traslacion
+                                  | coloreado 
+                                  | escalado
+                                  | dibujado'''
+	pass
+
+def p_rotacion(p):
+	'rotacion : Accion Identificador Reservado Valor Unidad Delimitador'
+	pass
+
+def p_traslacion(p):
+	'traslacion : Accion Reservado Atributo Identificador Reservado Valor Unidad Delimitador'
+	pass
+
+def p_coloreado(p):
+	'coloreado : Accion Identificador Reservado Color Delimitador'
+	pass
+
+def p_escalado(p):
+	'escalado : Accion Identificador Valor Unidad Delimitador'
+	pass
+
+def p_dibujado(p):
+	'dibujado : Accion Identificador Delimitador'
+	pass
+
+def p_error(p):
+	#print str(dir(p))
+	#print str(dir(cminus_lexer))
+	if VERBOSE:
+		if p is not None:
+			print "Error de sintaxis en la linea: " + str(p.lexer.lineno) 
+				#+ " Unexpected token  " + str(p.value)
+		else:
+			print "Syntax error at line: " + str(lexico.lexer.lineno)
+	else:
+		raise Exception('syntax', 'error')
+		
+
+parser = yacc.yacc()
+
+if __name__ == '__main__':
+
+	if (len(sys.argv) > 1):
+		fin = sys.argv[1]
+	else:
+		fin = 'ejemplos/cilindro.CG'
+
+	f = open(fin, 'r')
+	data = f.read()
+	print data
+	parser.parse(data, tracking=True)
+	
+
