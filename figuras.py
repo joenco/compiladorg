148dfe0871d0ce1906a97171d7d0d09ec8f29dcb
Jesus <jesus@jesus-M2420.(none)>
2014-03-14 12:09:26 -0430

    Graficando recta, rotar, escalar y trasladar

--- figuras.py
+++ figuras.py
@@ -1,6 +1,7 @@
 import turtle
 import math
 
+#Funcion principal
 def dibujar(simbolos):
   simbolos = simbolos
 
@@ -11,17 +12,25 @@ def dibujar(simbolos):
 
   plano2d()
 
+  #Tabla de simbolos
   print "Tabla de Simbolos: ",simbolos
+  #Identificadores procesados
   print "Indentificador\tTipo"
+  #Recorriendo la tabla de simbolos
   for a in simbolos:
     if (a[2]== "Punto"):
       print a[1]+"\t\t"+a[2]
       #Dibujar
       if (int(a[10]) == 1 ):
         punto(float(a[3]),float(a[4]),float(a[7]),float(a[8]),obtener_color(str(a[9])))
-
+    elif (a[2]=="Recta"):
+      if (int(a[10]) == 1 ):
+        # Atributos: x1, y1, x2, y2, rotar, escalar, trasladarx, trasladary, color
+        recta(obtener_x(a[3], simbolos),obtener_y(a[3], simbolos),obtener_x(a[4], simbolos),obtener_y(a[4], simbolos),int(a[5]),int(a[6]),float(a[7]),float(a[8]),obtener_color(a[9]))
+  
   turtle.exitonclick()
 
+#Dibujar plano 2D
 def plano2d():
   turtle.penup()
 
@@ -53,6 +62,7 @@ def plano2d():
     turtle.left(grados)
     turtle.forward(264) 
   
+#Dibujar punto
 def punto(x,y,tx,ty,color):
   x = x
   y = y
@@ -63,6 +73,72 @@ def punto(x,y,tx,ty,color):
   turtle.pendown()
   turtle.dot(20,color)
 
+#Plantilla recta
+def recta(x1, y1, x2, y2, rotar, escalar, tx, ty, color):
+  x1 = x1
+  y1 = y1
+  x2 = x2 
+  y2 = y2
+  rotar = rotar
+  escalar = escalar
+
+  #Trasladar recta
+  x1 = x1*44 + tx*44
+  x2 = x2*44 + tx*44
+  y1 = y1*44 + ty*44
+  y2 = y2*44 + ty*44 
+
+  #Calculos recta
+  angulo = int(math.degrees(math.atan2((y2 - y1) , (x2 - x1))))
+  punto_medio_x = (x1 + x2) / 2 
+  punto_medio_y = (y1 + y2) / 2
+  distancia = int (math.sqrt(((x2 -x1)**2)+((y2-y1)**2)))
+
+  print "x1, y1, x2, y2: ",x1,y1,x2,y2
+  print "Angulo: ",angulo 
+  print "Distancia",distancia
+
+  #Dibujar recta
+  turtle.penup()
+  turtle.setposition(x1,y1)
+  turtle.pendown()
+  turtle.color(color)
+  turtle.pensize(8)
+  turtle.setposition(x2,y2)
+
+  #Rotar recta
+  if rotar != 0:
+    turtle.color("#008080")
+    turtle.penup()
+    turtle.setposition(punto_medio_x,punto_medio_y)
+    turtle.setheading(0)
+    turtle.lt(angulo+rotar)
+    turtle.pendown()
+    turtle.forward(distancia/2)
+    turtle.penup()
+    turtle.setposition(punto_medio_x,punto_medio_y)
+    turtle.setheading(0)
+    turtle.lt(angulo+rotar+180)
+    turtle.pendown()
+    turtle.forward(distancia/2)
+
+  #Escalar recta
+  if escalar != 0:
+    turtle.color("#00FFFF")
+    turtle.penup()
+    turtle.setposition(punto_medio_x,punto_medio_y)
+    turtle.setheading(0)
+    turtle.lt(angulo)
+    turtle.pendown()
+    turtle.forward((distancia/2)*escalar)
+    turtle.penup()
+    turtle.setposition(punto_medio_x,punto_medio_y)
+    turtle.setheading(0)
+    turtle.lt(angulo+180)
+    turtle.pendown()
+    turtle.forward((distancia/2)*escalar)
+ 
+#Traducir color a Ingles
 def obtener_color(color):
   color = color
   if color == "azul" or color == "Azul":
@@ -84,3 +160,22 @@ def obtener_color(color):
   else:
     color = "black"
   return color
+
+#Extraer coordenada x de un punto
+def obtener_x(punto, simbolos):
+  simbolos = simbolos
+  punto = punto
+  for a in simbolos:
+    if (a[1]== punto):
+      return float(a[3])
+  return 0
+
+#Extraer coordenada y de un punto
+def obtener_y(punto, simbolos):
+  simbolos = simbolos
+  punto = punto
+  for a in simbolos:
+    if (a[1]== punto):
+      return float(a[4])
+  return 0
+
