af080261e738f1447275f98df5c865f4908a4181
joenco <libretecnologia@gmail.com>
2014-03-19 00:28:06 -0430

    funciona para hacer correr los jemplos de las tablas de simbolos

---
+++ palabras.py
@@ -0,0 +1,21 @@
+#! /usr/bin/emv python
+
+import tabladesimbolos as funcion
+
+palabras = ' '
+archivo = ['ejemplos/punto.CG']
+tipo=' '
+for texto in archivo:
+    f = open(texto, 'r')
+    data = f.read()
+    #palabras = data.split(' )
+    lineas = data.splitlines()
+    f.close()
+
+#result = funcion.tabla(data, lineas)
+#lineas = funcion.atributos(lineas, result[0])
+#print lineas
+h = funcion.simbolos(data, lineas)
+n = len(h)
+for i in h[1]:
+  print i
