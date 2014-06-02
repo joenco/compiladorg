dac8a1a6b91dc5fe0b324d8fcd6ee782948a80e0
joenco <libretecnologia@gmail.com>
2014-06-02 12:57:56 -0430

    actualizado

--- palabras.py
+++ palabras.py
@@ -3,7 +3,7 @@
 import tabladesimbolos as funcion
 
 palabras = ' '
-archivo = ['ejemplos/cilindro.CG']
+archivo = ['ejemplos/triangulo.CG']
 tipo=' '
 for texto in archivo:
     f = open(texto, 'r')
